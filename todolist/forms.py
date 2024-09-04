6a836f1deb211ec061cb4c7a8be76df08dc343b0
VVArgost <88936894+VVArgost@users.noreply.github.com>
2024-09-04 17:11:30 +0200

    Delete todolist directory

--- todolist/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class TodoListForm(forms.Form):
-    text = forms.CharField(max_length=4500,
-        widget=forms.TextInput(
-            attrs={'class' : 'form-control', 'placeholder' :'Enter todo e.g Grocery Shopping', 'aria-label' :'Todo', 'aria-describeby' : 'add-btn'}))
-
